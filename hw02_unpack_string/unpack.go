4e3602da24d619b93a78bf909830f415697ed17d
Aleksey Yanovskiy <yanov-sky@mail.ru>
2022-06-19 11:13:38 +0300

    Второе задание выполнено

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,45 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(encodedString string) (string, error) {
+	var builder strings.Builder
+	var runeParts = []rune(encodedString)
+	var lastRune rune = -1
+
+	for i := 0; i < len(encodedString); i++ {
+		if unicode.IsDigit(runeParts[i]) {
+			if lastRune == -1 {
+				return "", ErrInvalidString
+			} else {
+				multiplier, err := strconv.Atoi(string(runeParts[i]))
+				if err != nil {
+					return "", ErrInvalidString
+				}
+
+				builder.WriteString(strings.Repeat(string(lastRune), multiplier))
+				lastRune = -1
+			}
+		} else if unicode.IsLetter(runeParts[i]) {
+			if unicode.IsLetter(lastRune) {
+				builder.WriteString(string(lastRune))
+			}
+
+			lastRune = runeParts[i]
+		} else {
+			return "", ErrInvalidString
+		}
+	}
+
+	if unicode.IsLetter(lastRune) {
+		builder.WriteString(string(lastRune))
+	}
+
+	return builder.String(), nil
 }
