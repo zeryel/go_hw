aa9be4c06e3f405c2bdfadd2e694bf5af395ea6a
Aleksey Yanovskiy <yanov-sky@mail.ru>
2022-08-03 10:05:46 +0300

    Исправление ошибок линтера

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -11,29 +11,31 @@ var ErrInvalidString = errors.New("invalid string")
 
 func Unpack(encodedString string) (string, error) {
 	var builder strings.Builder
-	var runeParts = []rune(encodedString)
 	var lastRune rune = -1
 
+	runeParts := []rune(encodedString)
+
 	for i := 0; i < len(encodedString); i++ {
-		if unicode.IsDigit(runeParts[i]) {
+		switch {
+		case unicode.IsDigit(runeParts[i]):
 			if lastRune == -1 {
 				return "", ErrInvalidString
-			} else {
-				multiplier, err := strconv.Atoi(string(runeParts[i]))
-				if err != nil {
-					return "", ErrInvalidString
-				}
-
-				builder.WriteString(strings.Repeat(string(lastRune), multiplier))
-				lastRune = -1
 			}
-		} else if unicode.IsLetter(runeParts[i]) {
+
+			multiplier, err := strconv.Atoi(string(runeParts[i]))
+			if err != nil {
+				return "", ErrInvalidString
+			}
+
+			builder.WriteString(strings.Repeat(string(lastRune), multiplier))
+			lastRune = -1
+		case unicode.IsLetter(runeParts[i]):
 			if unicode.IsLetter(lastRune) {
 				builder.WriteString(string(lastRune))
 			}
 
 			lastRune = runeParts[i]
-		} else {
+		default:
 			return "", ErrInvalidString
 		}
 	}
